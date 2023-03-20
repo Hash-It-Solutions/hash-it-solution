0c5857e8752276edea49f289a9dc977f97d5a934
Swathikrishna US <swathikrishnaus2015@gmail.com>
2023-03-20 18:34:41 +0530

    created pending pages

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-var myDiv = document.getElementById("my-div");
-myDiv.addEventListener("mouseover", function() {
-  myDiv.innerHTML = "New content";
-});
+// var myDiv = document.getElementById("my-div");
+// myDiv.addEventListener("mouseover", function() {
+//   myDiv.innerHTML = "New content";
+// });
 
-myDiv.addEventListener("mouseout", function() {
-  myDiv.innerHTML = "Original content";
-});
+// myDiv.addEventListener("mouseout", function() {
+//   myDiv.innerHTML = "Original content";
+// });
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 100) {
+      $(".navbar").addClass("navbar-scrolled");
+    } else {
+      $(".navbar").removeClass("navbar-scrolled");
+    }
+  });
+  
